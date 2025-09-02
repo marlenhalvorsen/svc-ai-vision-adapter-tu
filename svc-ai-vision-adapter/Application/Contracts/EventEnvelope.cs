5368b44b3869fc54b2f1f2b8db86b7161cf77c3c
marlenhalvorsen <marlenhalvorsen@gmail.com>
2025-09-02 11:15:08 +0200

    Implemented Eventhandling and Unit test for RecognitionService

---
+++ svc-ai-vision-adapter/Application/Contracts/EventEnvelope.cs
@@ -0,0 +1,12 @@
+﻿namespace svc_ai_vision_adapter.Application.Contracts
+{
+    /// <summary>
+    /// A generic event "envelope" for HTTP-based eventing
+    /// </summary>
+    public record EventEnvelope<T>(
+        string Type,
+        string Id,
+        string? ReplyTo,
+        T Data 
+    );
+}
