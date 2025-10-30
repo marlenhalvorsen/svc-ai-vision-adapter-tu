352150b21850bf457c862d04f50edf9d5d147312
marlenhalvorsen <marlenhalvorsen@gmail.com>
2025-10-30 15:17:14 +0100

    Added unintentionally ignored file. Explicitly added exceptions to gitignore to include files in Out file in ports.

---
+++ svc-ai-vision-adapter/Application/Ports/Out/IRecognitionCompletedPublisher.cs
@@ -0,0 +1,12 @@
+﻿using svc_ai_vision_adapter.Application.Contracts;
+using svc_ai_vision_adapter.Infrastructure.Adapters.Kafka.Models;
+
+///When a recognition is completed the event is published by 
+///RecognitionCompletedKafkaProducer to an external messagebroker. 
+namespace svc_ai_vision_adapter.Application.Ports.Out
+{
+    public interface IRecognitionCompletedPublisher
+    {
+        Task PublishAsync(RecognitionResponseDto response, CancellationToken ct);
+    }
+}
