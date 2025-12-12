c9ce17ec3bbfb492f4e47622eefe5f12c0608377
marlenhalvorsen <marlenhalvorsen@gmail.com>
2025-12-12 09:34:55 +0100

    fix: added retry and timeout for external call to GoogleVision

---
+++ svc-ai-vision-adapter/Infrastructure/Adapters/GoogleVision/IGoogleVisionClient.cs
@@ -0,0 +1,11 @@
+﻿using Google.Cloud.Vision.V1;
+
+namespace svc_ai_vision_adapter.Infrastructure.Adapters.GoogleVision
+{
+    internal interface IGoogleVisionClient
+    {
+        Task<BatchAnnotateImagesResponse> BatchAnnotateAsync(
+            BatchAnnotateImagesRequest request,
+            CancellationToken ct);
+    }
+}
